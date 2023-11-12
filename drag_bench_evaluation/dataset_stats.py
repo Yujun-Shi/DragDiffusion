49beef5a0911e10f7b3f19107f6c3d7381db3a94
Yujun-Shi <shiyujun1016@163.com>
2023-11-12 20:53:09 +0800

    update dataset stats scripts

---
+++ drag_bench_evaluation/dataset_stats.py
@@ -0,0 +1,57 @@
+# *************************************************************************
+# Copyright (2023) Bytedance Inc.
+#
+# Copyright (2023) DragDiffusion Authors 
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); 
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at 
+#
+#     http://www.apache.org/licenses/LICENSE-2.0 
+#
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# See the License for the specific language governing permissions and 
+# limitations under the License. 
+# *************************************************************************
+
+import os
+import numpy as np
+import pickle
+
+import sys
+sys.path.insert(0, '../')
+
+
+if __name__ == '__main__':
+    all_category = [
+        'art_work',
+        'land_scape',
+        'building_city_view',
+        'building_countryside_view',
+        'animals',
+        'human_head',
+        'human_upper_body',
+        'human_full_body',
+        'interior_design',
+        'other_objects',
+    ]
+
+    # assume root_dir and lora_dir are valid directory
+    root_dir = 'drag_bench_data'
+
+    num_samples, num_pair_points = 0, 0
+    for cat in all_category:
+        file_dir = os.path.join(root_dir, cat)
+        for sample_name in os.listdir(file_dir):
+            if sample_name == '.DS_Store':
+                continue
+            sample_path = os.path.join(file_dir, sample_name)
+
+            # load meta data
+            with open(os.path.join(sample_path, 'meta_data.pkl'), 'rb') as f:
+                meta_data = pickle.load(f)
+            points = meta_data['points']
+            num_samples += 1
+            num_pair_points += len(points) // 2
